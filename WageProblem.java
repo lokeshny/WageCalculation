7dfd67d91249e9da393d393ebde413782723b1a1
lokeshny <lokeshny97@gmail.com>
2022-04-09 18:31:50 +0530

    added daily emplopyee wage

--- WageProblem.java
+++ WageProblem.java
@@ -3,14 +3,18 @@ package Pacticeproblem;
 public class WageProblem {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to wage calculation");
         final int FULL_TIME = 1;
+        final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
+        System.out.println("Welcome to wage calculation");
         if (empType == FULL_TIME) {
             System.out.println("Employee is Present");
 
         } else {
             System.out.println("Employee is Absent");
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
